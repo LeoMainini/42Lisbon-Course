1b9f0a977966538c6955992f9d273231c49758fe
LeoMainini <leonardo.apple.mainini@gmail.com>
2022-02-15 21:48:55 +0000

    demalloced memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: leferrei <leferrei@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 16:52:05 by leferrei          #+#    #+#             */
-/*   Updated: 2022/02/07 19:33:37 by leferrei         ###   ########.fr       */
+/*   Updated: 2022/02/15 21:48:04 by leferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int		index;
-	char	*temp;
-	char	**b_dest;
-	char	**b_src;
+	int	i;
 
-	b_dest = (char **)&dest;
-	b_src = (char **)&src;
-	temp = (char *)malloc(n * (sizeof(*temp)));
-	index = -1;
-	while (++index < n)
-		temp[index] = (*b_src)[index];
-	index = -1;
-	while (++index < n)
-		(*b_dest)[index] = temp[index];
-	return (&((*b_dest)[0]));
+	if (dest > src)
+	{
+		i = (int)n;
+		while (--i >= 0)
+			((char *)dest)[i] = ((char *)src)[i];
+	}
+	else
+	{
+		i = -1;
+		while (++i < (int)n)
+			((char *)dest)[i] = ((char *)src)[i];
+	}
+	return (dest);
 }
 
 int main(void)
@@ -40,9 +40,8 @@ int main(void)
 	char *leo = (char *)malloc(10);
 	char *lib = (char *)malloc(10);
 
-	printf("return leo = %s\n", ft_memmove(leo, test, 1));
-	printf("return lib = %s\n", memmove(lib, test, 1));
+	printf("return leo = %s\n", (char *)ft_memmove(leo, test, 5));
+	printf("return lib = %s\n", (char *)memmove(lib, test, 5));
 	printf("leo = %s\n", leo);
 	printf("lib = %s\n", lib);
-	Bom dia ? como guardar este frase??
 }

b48a7aa2540b28ce1f1f3a8c0a91890065fb8543
Leonardo osvaldo Ferreira mainini <leferrei@c1r17s6.42lisboa.com>
2022-04-21 16:37:45 +0100

    add intra submission

---
+++ get_next_line/vogsphere-git/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leferrei <leferrei@student.42lisboa>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 19:05:16 by leferrei          #+#    #+#             */
+/*   Updated: 2022/04/13 19:05:30 by leferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_sj(char *read_data, char *buf);
+int		clean_buf(char *buf);
+
+#endif

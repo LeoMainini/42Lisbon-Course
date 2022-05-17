dd65eec3925d18eeb677f4c41c3ef0e14e53f6c9
Leonardo osvaldo Ferreira mainini <leferrei@c1r17s6.42lisboa.com>
2022-05-17 14:27:25 +0100

    add fractol base

---
+++ fractol/fractol.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leferrei <leferrei@student.42lisboa>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 14:16:47 by leferrei          #+#    #+#             */
+/*   Updated: 2022/05/17 14:23:47 by leferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx/mlx.h"
+
+int main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
---
+++ fractol/ft_printf/ft_putstr_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leferrei <leferrei@student.42lisboa>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 19:10:00 by leferrei          #+#    #+#             */
+/*   Updated: 2022/03/15 17:20:26 by leferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putstr_fd(char *s, int fd, int *counter)
+{
+	if (s != NULL)
+		while (*s)
+			ft_putchar_fd(*(s++), fd, counter);
+	else
+		ft_putstr_fd("(null)", 1, counter);
+}
